e5cf0be17f225315ea145a37a32137de2ed5ec3c
Lucas259746 <lucas.rozotte@laplateforme.io>
2026-06-05 09:36:05 +0200

    changement de node.js et de react-scripts à vite

--- MonHsrVite/src/components/RelicSection.jsx
+++ MonHsrVite/src/components/RelicSection.jsx
@@ -1,4 +1,4 @@
-import RelicCard from './RelicCard';
+import RelicCard from "./RelicCard";
 
 function RelicSection({ activeCharacter }) {
   if (!activeCharacter) {
@@ -7,17 +7,24 @@ function RelicSection({ activeCharacter }) {
 
   return (
     <div className="relic-section box mt-5">
-      <h3 className="title is-5 font-orbitron has-text-gold mb-4">Reliques d'équipement</h3>
+      <h3 className="title is-5 font-orbitron has-text-gold mb-4">
+        Reliques d'équipement
+      </h3>
       {activeCharacter.relics?.length > 0 ? (
         <div className="columns is-multiline">
           {activeCharacter.relics.map((relic) => (
-            <div key={relic.id || relic.name} className="column is-half-tablet is-one-third-desktop">
+            <div
+              key={relic.id || relic.name}
+              className="column is-half-tablet is-one-third-desktop"
+            >
               <RelicCard relic={relic} />
             </div>
           ))}
         </div>
       ) : (
-        <p className="has-text-grey-light">Aucune relique détectée en vitrine.</p>
+        <p className="has-text-grey-light">
+          Aucune relique détectée en vitrine.
+        </p>
       )}
     </div>
   );
